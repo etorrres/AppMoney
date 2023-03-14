5f162695359a1da4166f821ef3f89b51bfa4a1c3
Edisson <edisson072011@gmail.com>
2023-03-13 23:37:05 -0600

    binding en MainActivity

--- app/src/main/java/tech/edisystem/appmoney/MainActivity.java
+++ app/src/main/java/tech/edisystem/appmoney/MainActivity.java
@@ -3,12 +3,16 @@ package tech.edisystem.appmoney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import tech.edisystem.appmoney.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
     }
 }
