d8e25549b1c09619ad35d31c10ca5877fed8bea6
Edisson <edisson072011@gmail.com>
2023-03-17 23:10:45 -0600

    Creacion de Vistas

---
+++ app/src/main/java/tech/edisystem/appmoney/mov_nuevo_Activity.java
@@ -0,0 +1,14 @@
+package tech.edisystem.appmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mov_nuevo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mov_nuevo);
+    }
+}
