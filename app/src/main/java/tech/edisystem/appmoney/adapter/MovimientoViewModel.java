61a450a45ed58a470377aed3bc02a4ca9a995498
etorrres <79731703+etorrres@users.noreply.github.com>
2023-03-17 23:56:01 -0600

    Actualizacion de ViewModel

--- app/src/main/java/tech/edisystem/appmoney/adapter/MovimientoViewModel.java
+++ app/src/main/java/tech/edisystem/appmoney/adapter/MovimientoViewModel.java
@@ -1,7 +1,39 @@
 package tech.edisystem.appmoney.adapter;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import tech.edisystem.appmoney.database.MovimientoRepository;
+import tech.edisystem.appmoney.movimientoslist.entity.Egresos;
+import tech.edisystem.appmoney.movimientoslist.entity.Ingresos;
 
 public class MovimientoViewModel extends AndroidViewModel {
+
+private MovimientoRepository repository;
+
+private final LiveData<List<Ingresos>> ingresoDataset;
+
+
+
+    public MovimientoViewModel(@NonNull Application application) {
+        super(application);
+        repository = new MovimientoRepository(application);
+        this.ingresoDataset = repository.getIngresosDataset();
+    }
+
+    public void insert(Ingresos nuevo){repository.insert(nuevo);}
+
+    public void delete(Ingresos eliminar){
+        repository.delete(eliminar);
+    }
+
+    public void deleteAll(){
+        repository.deleteAll();
+    }
 
 }
--- app/src/main/java/tech/edisystem/appmoney/database/MovimientoRepository.java
+++ app/src/main/java/tech/edisystem/appmoney/database/MovimientoRepository.java
@@ -7,18 +7,41 @@ import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
+import tech.edisystem.appmoney.movimientoslist.entity.Egresos;
 import tech.edisystem.appmoney.movimientoslist.entity.Ingresos;
 import tech.edisystem.appmoney.movimientoslist.entity.MovimientoDao;
 
 public class MovimientoRepository {
 
     private MovimientoDao movimientoDao;
-    private LiveData<List<Ingresos>> movimientoDataset;
+    private LiveData<List<Ingresos>> ingresoDataset;
+    private LiveData<List<Ingresos>> egresosDataset;
 
     public MovimientoRepository(Application app){
         MovimientosDatabase db = MovimientosDatabase.getDatabase(app);
         movimientoDao = db.movimientoDao();
-        movimientoDataset = movimientoDao.mostrarIngresos();
+        ingresoDataset = movimientoDao.mostrarIngresos();
     }
 
+    public LiveData<List<Ingresos>> getIngresosDataset() {
+        return ingresoDataset;
+    }
+
+    public void insert(Ingresos nuevo){
+        MovimientosDatabase.databaseWriteExecutor.execute(() ->{
+            movimientoDao.insert(nuevo);
+        });
+    }
+
+    public void delete(Ingresos eliminar){
+        MovimientosDatabase.databaseWriteExecutor.execute(() ->{
+            movimientoDao.delete(eliminar);
+        });
+    }
+
+    public void deleteAll(){
+        MovimientosDatabase.databaseWriteExecutor.execute(() ->{
+            movimientoDao.deleteAllIngresos();
+        });
+    }
 }
